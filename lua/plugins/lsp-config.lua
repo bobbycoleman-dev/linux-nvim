15af2d973e7351c4472647813bdefab269ff81b0
Bobby Coleman <bobbycoleman.dev@gmail.com>
2024-11-02 08:31:52 -0500

    lsp update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -71,7 +71,7 @@ return { -- LSP Configuration & Plugins
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
-      tsserver = {},
+      ts_ls = {},
       --
       html = {},
       tailwindcss = {},
